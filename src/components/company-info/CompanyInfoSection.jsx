18ec8332aa0f027f71d9d76417b5e92db92da26e
Slim <vintio234@gmail.com>
2025-04-15 20:47:39 +0200

    Fixes: add an opacity for the cards for a better visual appearance -

--- src/components/company-info/CompanyInfoSection.jsx
+++ src/components/company-info/CompanyInfoSection.jsx
@@ -4,25 +4,25 @@ const CompanyInfoSection = () => {
   return (
     <section className="relative py-10 bg-[#043A53]">
       <div className="grid grid-cols-2 grid-rows-2 text-center text-gray-700 px-4 md:px-10">
-        <div className="p-12 flex items-center justify-center text-start hover:z-50 bg-white transition-all duration-300 border border-gray-100">
+        <div className="p-12 flex items-center justify-center text-start hover:z-50 bg-white hover:bg-white/80 transition-all duration-300 border border-gray-100">
           <p>
             We are a leading manufacturer in East Africa, focused on delivering
             sustainable and eco-friendly paper products to the market.
           </p>
         </div>
-        <div className="p-12 flex items-center justify-center text-start hover:z-50 bg-white transition-all duration-300 border border-gray-100">
+        <div className="p-12 flex items-center justify-center text-start hover:z-50 bg-white hover:bg-white/80 transition-all duration-300 border border-gray-100">
           <p>
             Our mission is to revolutionize the paper industry with innovation,
             quality, and environmental consciousness.
           </p>
         </div>
-        <div className="p-12 flex items-center justify-center text-start hover:z-50 bg-white transition-all duration-300 border border-gray-100">
+        <div className="p-12 flex items-center justify-center text-start hover:z-50 bg-white hover:bg-white/80 transition-all duration-300 border border-gray-100">
           <p>
             We envision a world where recycled materials power the paper
             industry, reducing deforestation globally.
           </p>
         </div>
-        <div className="p-12 flex items-center justify-center text-start hover:z-50 bg-white transition-all duration-300 border border-gray-100">
+        <div className="p-12 flex items-center justify-center text-start hover:z-50 bg-white hover:bg-white/80 transition-all duration-300 border border-gray-100">
           <p>
             Integrity, sustainability, and excellence guide all our business
             decisions and community interactions.
