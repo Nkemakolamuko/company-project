84ed47a683a5473d0d092ff3b10c8fe748777f27
Slim <vintio234@gmail.com>
2025-04-20 01:16:58 +0200

    Update: add mobile responsiveness for the sub companies page -

--- src/components/subcompanies/HeroSection.jsx
+++ src/components/subcompanies/HeroSection.jsx
@@ -1,12 +1,12 @@
 export default function HeroSection({ title, description,img }) {
   return (
-    <section className="py-44 w-full bg-cover relative" style={{
+    <section className="py-24 md:py-44 w-full bg-cover relative" style={{
       backgroundImage: `url(${img})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
     }}>
       {/* <div className="absolute bg-gray-800/50 w-full h-full top-0"></div> */}
-      <div className="bg-black/50 max-w-lg ml-16 flex flex-col sm:p-4 text-white z-50">
+      <div className="bg-black/50 max-w-lg mx-4 md:ml-16 flex flex-col p-4 text-white z-50">
       <h1 className="text-4xl font-bold">{title}</h1>
       <p className="mt-4 text-gray-100">{description}</p>
       </div>
